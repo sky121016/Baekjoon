9309d6502bf13cf63ddc86d56e457d7b50dcc2d0
sky121016 <102947171+sky121016@users.noreply.github.com>
2023-08-02 20:59:27 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+
+    if(y > 0){
+        if(x > 0){
+            cout<<"1";
+        }else{
+            cout<<"2";
+        }
+    }else{
+        if(x > 0){
+            cout<<"4";
+        }else{
+            cout<<"3";
+        }
+    }
+}
