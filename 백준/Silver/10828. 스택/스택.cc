ea2974097646a6c531941ca0b2b0fd0f37993286
sky121016 <102947171+sky121016@users.noreply.github.com>
2023-12-27 16:19:51 +0900

    [Silver IV] Title: 스택, Time: 324 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,55 +1,68 @@
-#include <iostream>
-#include <string>
-
+// 10828
+#include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
-
-int main()
-{
-    string command;             // 명령
-
-    int size;                   // 명령의 수
-    cin >> size;
-
-    int top = 0;
-
-    int arr[10000];             // 스택 배열
     
+int n;
+int cnt;
+int num;
+vector<int> arr;
 
-    for (int i = 0; i < size; i++){
-        cin >> command;
+int main(){
+    cin >> n;
 
-        if (command == "push"){
-            cin >> arr[top];
-            top++;
-        } 
+
+    string cmd;
+    while(cnt < n){
+        cin >> cmd;
         
-        else if (command == "pop"){
-            if (top > 0){
-                top--;
-                cout << arr[top] << endl;
-            } else {
-                cout<<"-1"<<endl;
+        if(cmd == "push"){
+
+            cin >> num;
+            arr.push_back(num);
+
+        } else if(cmd == "pop"){
+
+            int size = arr.size();
+
+            if(size <= 0){
+                cout << "-1\n";
+            }else{
+                cout << arr[size - 1] << endl;
+                arr.pop_back();
             }
-        } 
-        
-        else if (command == "size"){
-            cout << top << endl;
-        } 
-        
-        else if (command == "empty"){
-            if (top == 0){
-                cout << "1" << endl;
-            } else {
-                cout << "0" << endl;
+
+        } else if(cmd == "size"){
+
+            int size = arr.size();
+
+            if(arr.empty()){
+                cout << "0\n";
+            }else{
+                cout<<arr.size()<<endl;
             }
+
+        } else if(cmd == "empty"){
+
+            if(arr.empty()){
+                cout<<"1\n";
+            }else{
+                cout<<"0\n";
+            }
+
+        } else if(cmd == "top"){
+             
+            int size = arr.size();
+
+            if(arr.empty()){
+                cout<<"-1\n";
+            }else{
+                cout<<arr[size-1]<<endl;
+            }
+            
         }
-        
-        else if (command == "top"){
-            if (top>0){
-                cout << arr[top-1] << endl;
-            } else {
-                cout << "-1" << endl;
-            }
-        }
+
+        cnt++;
     }
 }
