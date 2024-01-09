1c8e1e4c7a89a801ca8310b0c76b086c4b650666
sky121016 <102947171+sky121016@users.noreply.github.com>
2024-01-09 15:51:26 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -1,30 +1,30 @@
-// 11727 2n 타일링
+// 11727
 #include<iostream>
-
 using namespace std;
 
-long long dp[1010];
+int dp[1001];
+int N;
 
-int solution(int n){
-    if(n == 1){
-        return 1;
-    }else if(n == 2){
-        return 3;
-    }
-    if(dp[n] != 0){
+int tile(int n){
+    if(dp[n]!=0){
         return dp[n];
     }
 
-    return dp[n] = (solution(n - 1) + 2 * solution(n - 2)) % 10007;
-}
+    if(n==1){
+        return 1;
+    }else if(n==2){
+        return 3;
+    }
 
+    return dp[n] = (tile(n - 1) + 2 * tile(n - 2)) % 10007;
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    cin >> n;
-    cout << solution(n);
+    cin >> N;
+
+    cout << tile(N);
 }
