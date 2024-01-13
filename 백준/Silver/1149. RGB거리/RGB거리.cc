8fbaab80be3f33317db4ab9a61e038d3641ecaf9
sky121016 <102947171+sky121016@users.noreply.github.com>
2024-01-13 14:10:15 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,51 +1,41 @@
-// 1149 RGB 거리
+// 1149
 #include<iostream>
+#include<queue>
 #include<vector>
 #include<algorithm>
 using namespace std;
 
 int n;
-int cost[1001][3];
 int dp[1001][3];
-int ans;
-
-void RGB(){
-
-    for(int i = 1; i<=n; i++){
-        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
-        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
-        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2];
-    }
-
-
-    ans = dp[n][0];
-    for(int i = 1; i<3; i++){
-        if(ans>=dp[n][i]){
-            ans = dp[n][i];
-        }
-    }
-
-
-}
-
+int rgb[1001][3];
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin>>n;
+    cin >> n;
 
-
-    for(int i = 1; i<=n; i++){
+    for(int i = 0; i<n; i++){
         for(int j = 0; j<3; j++){
-            cin>>cost[i][j];
+            cin >> rgb[i][j];
         }
     }
 
-    RGB();
+    for(int i = 0; i<3; i++){
+        dp[0][i] = rgb[0][i];
+    }
 
-    cout<<ans;
+    for(int i = 1; i<n; i++){
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb[i][0];
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb[i][1];
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb[i][2];
+    }
 
+    int M;
+    M = min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
 
+    cout << M;
 }
+
+
