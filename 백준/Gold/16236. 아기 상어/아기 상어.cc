21e6e02d08e7f74979d0dc32ac84c780661ed12d
sky121016 <102947171+sky121016@users.noreply.github.com>
2024-01-22 23:24:39 +0900

    [Gold III] Title: 아기 상어, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,36 +1,40 @@
-// 16236 아기상어
-#include<iostream>
-#include<vector>
-#include<queue>
-#include<algorithm>
+// 16236
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int sharkX, sharkY;
-int sharkSize=2;
-int eatCnt;
-int n;
-int ans;
+int curX, curY;
 
-class Map{
+
+class Node{
 public:
-    int x,y;
+    int x, y;
     int value;
+
     int dis;
 
-    void setMap(int y, int x, int value){
+    void setNode(int y, int x, int v){
         this->y = y;
         this->x = x;
-        this->dis = -1;
-
-        if(value <= 6){
-            this->value = value;
-        }else if(value == 9){
-            this->value = 0;
-            sharkY = y;
-            sharkX = x;
+        if(v==9){
+            value = 0;
+            curY = y;
+            curX = x;
+        }else{
+            value = v;
         }
+        dis = -1;
+
+    }
+
+    void initNode(){
+        dis = -1;
     }
 };
+
 
 int dir[4][2] = {
     {-1, 0},
@@ -39,64 +43,69 @@ int dir[4][2] = {
     {0, -1}
 };
 
-Map map[21][21];
+int n;
+Node node[21][21];
+
+int sharkSize;
+int ans;
+int eatCnt;
 
 int minDis;
-int minY, minX;
+int minX, minY;
+
 
 bool canVisit(int ny, int nx){
-    return (map[ny][nx].dis<0 && sharkSize >= map[ny][nx].value && nx >= 0 && nx < n && ny >= 0 && ny < n);
+    return (node[ny][nx].dis < 0 && node[ny][nx].value <= sharkSize && ny < n && ny >= 0 && nx < n && nx >= 0);
 }
 
 bool canEat(int ny, int nx){
-    return (map[ny][nx].value != 0 && sharkSize > map[ny][nx].value);
+    return (node[ny][nx].value < sharkSize && node[ny][nx].value != 0);
 }
 
 
-
 void bfs(){
-    queue<Map> q;
-    map[sharkY][sharkX].dis = 0;                // 시작위치
-    q.push(map[sharkY][sharkX]);
+    queue<Node> q;
+    Node v;
+    node[curY][curX].dis = 0;
 
-    int nx, ny;
-    Map v;
+    q.push(node[curY][curX]);
 
     while(!q.empty()){
         v = q.front();
         q.pop();
 
+        int ny, nx;
         for(int i = 0; i<4; i++){
             ny = v.y + dir[i][0];
             nx = v.x + dir[i][1];
 
-            if(canVisit(ny, nx)){
-                map[ny][nx].dis = v.dis + 1;
+            if (canVisit(ny, nx)){
+                node[ny][nx].dis = v.dis + 1;
 
-                if(canEat(ny, nx)){                                 // 물고기가 있고 먹을 수 있는 크기라면 가까운지 확인해야함
-
-                    if(minDis > map[ny][nx].dis){                   // 더 가깝다면 min값 바꾸기
-                        minDis = map[ny][nx].dis;
+                if (canEat(ny, nx)){
+                    if(minDis > node[ny][nx].dis){
+                        minDis = node[ny][nx].dis;
                         minY = ny;
                         minX = nx;
-                    }else if(minDis == map[ny][nx].dis){            // 거리가 같다면 맨 위, 맨 왼쪽 물고기 찾기
-                        if(minY > ny){              // 맨 위
+
+                    }else if(minDis == node[ny][nx].dis){
+                        if(minY > ny){
                             minY = ny;
                             minX = nx;
-                        }else if(minY == ny){       // y가 같다면
-                            if(minX > nx){          // 맨 왼쪽 (x가 작은 거 선택)
+                        }else if(minY == ny){
+                            if (minX > nx){
                                 minY = ny;
                                 minX = nx;
                             }
                         }
                     }
                 }
-                q.push(map[ny][nx]);
-            }   
+                q.push(node[ny][nx]);
+            }
         }
     }
+    
 }
-
 
 
 int main(){
@@ -104,45 +113,48 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin>>n;
+    cin >> n;
 
     int temp;
+    sharkSize = 2;
+
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
-            cin>>temp;
-            map[i][j].setMap(i, j, temp);
+            cin >> temp;
+            node[i][j].setNode(i, j, temp);
         }
     }
 
+
     while(true){
-        minDis = 500;       // 가장 가까운 물고기까지 거리
-        minY = 21;          // 가장 가까운 물고기의 y
-        minX = 21;          // 가장 가까운 물고기의 x
+        minDis = 500;
+        minX = 21;
+        minY = 21;
 
         for(int i = 0; i<n; i++){
             for(int j = 0; j<n; j++){
-                map[i][j].dis = -1;         // 거리 초기화
+                node[i][j].initNode();
             }
         }
-        
+
         bfs();
 
-        if(minX != 21 && minY != 21){       // 먹을 물고기 찾음
-            ans += map[minY][minX].dis;     
-            eatCnt++;
+        if(minX != 21 && minY != 21){
+            ans += minDis;
+            node[minY][minX].value = 0;
+            eatCnt ++;
 
-            map[minY][minX].value = 0;      // 빈 칸으로
+            curX = minX;
+            curY = minY;
 
-            sharkY = minY;                  // 상어 위치 이동
-            sharkX = minX;
-
-            if (eatCnt >= sharkSize){       // 크기 키우기
+            if(eatCnt == sharkSize){
                 eatCnt = 0;
                 sharkSize++;
             }
         }else{
             break;
         }
+
     }
 
     cout << ans;
