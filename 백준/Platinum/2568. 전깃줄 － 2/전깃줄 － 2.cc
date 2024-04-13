b177513da88e9625fc286ae255964beb91da638f
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-04-13 22:41:39 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 36 ms, Memory: 4964 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
@@ -1,45 +1,45 @@
-
-
-// 2568
+// 12015
 #include<iostream>
 #include<vector>
 #include<queue>
 #include<algorithm>
 #include<string>
+#include<cmath>
+#include<stack>
+
 using namespace std;
 
-int N;
-vector<pair<int, int> > LIS;
-vector<pair<int, int> > a;
-bool line[500001];
-int l, r, mid;
-vector<int> p;
+int n;
+vector<pair<int,int> > line;
+vector<int> LIS;
+vector<int> P;
+int l, r, m;
 vector<int> ans;
 
-void BS(int n){
-    if(LIS.size() == 0){
-        LIS.push_back(make_pair(a[n].first, a[n].second));
-        p.push_back(0);
-    }else if(a[n].second > LIS[LIS.size() - 1].second){
-        LIS.push_back(make_pair(a[n].first, a[n].second));
 
-        p.push_back(LIS.size()-1);
+void BS(int num){
+    if(LIS.size() == 0){
+        LIS.push_back(num);
+        P.push_back(0);
+    }else if(LIS[LIS.size()-1] < num){
+        LIS.push_back(num);
+        P.push_back(LIS.size()-1);
     }else{
         l = 0;
-        r = LIS.size() - 1;
+        r = LIS.size()-1;
 
-        while(l < r){
-            mid = (l + r) / 2;
-
-            if (a[n].second < LIS[mid].second){
-                r = mid;
+        while(l<r){
+            m = (l + r) / 2;
+            
+            if(num <= LIS[m]){
+                r = m;
             }else{
-                l = mid + 1;
+                l = m + 1;
             }
         }
-        LIS[l].first = a[n].first;
-        LIS[l].second = a[n].second;
-        p.push_back(l);
+
+        LIS[l] = num;
+        P.push_back(l);
     }
 }
 
@@ -48,41 +48,36 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> N;
+    cin >> n;
 
-    int x, y;
-    for(int i = 0; i<N; i++){
-        cin >> x >> y;
-        a.push_back(make_pair(x, y));
+
+    int a, b;
+    for(int i = 0; i<n; i++){
+        cin >> a >> b;
+        line.push_back(make_pair(a, b));
+    }
+    
+    sort(line.begin(), line.end());
+
+    for(int i = 0; i<line.size(); i++){
+        BS(line[i].second);
     }
 
 
-    sort(a.begin(), a.end());
 
+    int cnt = LIS.size()-1;
 
-    for(int i = 0; i<N; i++){
-        BS(i);
-    }
-
-    cout << N - LIS.size() << "\n";
-
-    int cnt = LIS.size() - 1;
-
-    for(int i = N-1; i>=0; i--){
-        if(p[i] == cnt){
-            cnt --;
+    for(int i = n-1; i>=0; i--){
+        if(P[i] == cnt){
+            cnt--;
             continue;
         }
-        ans.push_back(a[i].first);
+        ans.push_back(line[i].first);
     }
 
-  
+    cout << n - LIS.size() << "\n";
+
     for(int i = ans.size()-1; i>=0; i--){
         cout << ans[i] << "\n";
     }
-
-
-
 }
-
-
