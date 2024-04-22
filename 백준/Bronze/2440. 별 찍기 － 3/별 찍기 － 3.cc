4f714e9a5de321ccdb27bb68ce1219b67bc6689e
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-04-22 23:50:19 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+int n;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    cin>>n;
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+ 
+}
