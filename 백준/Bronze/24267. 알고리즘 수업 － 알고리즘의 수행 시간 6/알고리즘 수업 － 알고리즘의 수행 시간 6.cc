a5f6bff5a9d641449a82e4d96548584e05cb51e6
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-06-08 22:36:00 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,26 @@
+// 11657
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include <climits>
+
+using namespace std;
+
+long long n;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    cout << n * (n - 1) * (n - 2) / 6 << "\n";
+    cout<<3;
+}
+
