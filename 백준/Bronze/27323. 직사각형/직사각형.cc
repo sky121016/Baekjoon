3d1c893cd191a260133bb620a01b2064ed5d131f
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-10-25 23:19:12 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,28 @@
+
+
+
+
+// 17265
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cstdlib>
+#include<algorithm>
+using namespace std;
+
+int a, b;
+
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b;
+
+    cout << a * b;
+}
