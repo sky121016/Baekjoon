c09cf1ffd6665413f0843e91f0c20d5288da0e8a
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-10-29 20:27:41 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,27 @@
+// 2357
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cstdlib>
+#include<algorithm>
+using namespace std;
+
+int n;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    cout << n * (n - 1);
+}
+
+
+
+
+
