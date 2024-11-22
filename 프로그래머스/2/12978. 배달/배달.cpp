8bbb24e51700f254596cd220d4c7ce565de18f44
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-11-22 23:53:17 +0900

    [level 2] Title: 배달, Time: 0.23 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,45 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+
 using namespace std;
 
-int dis[51][51];
+vector<pair<int, int> > adj[51];
 int INF = 99999999;
 int cnt;
+int dis[51];
+
+void shortestPath(){
+    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
+    
+    dis[1] = 0;
+    pq.push(make_pair(0, 1));
+    
+
+    int curr, curr_cost;
+    while(!pq.empty()){
+        curr_cost = pq.top().first;
+        curr = pq.top().second;
+        pq.pop();
+    
+        int next, nCost;
+        
+        for(int i = 0; i<adj[curr].size(); i++){
+            nCost = adj[curr][i].first;
+            next = adj[curr][i].second;
+            
+            if(dis[curr] + nCost < dis[next]){
+                dis[next] = curr_cost + nCost;
+                pq.push(make_pair(dis[next], next));
+            }
+        }
+        
+    }
+    
+}
 
 int solution(int N, vector<vector<int> > road, int K) {
-    
-    
-    for(int i = 1; i<=N; i++){
-        for(int j = 1; j<=N; j++){
-            dis[i][j] = INF;
-        }
-        dis[i][i] = 0;
-    }
     
     for(int i = 0; i<road.size(); i++){
         int a = road[i][0];
         int b = road[i][1];
         int c = road[i][2];
         
-        dis[a][b] = min(dis[a][b], c);
-        dis[b][a] = min(dis[b][a], c);
+        adj[a].push_back(make_pair(c, b));
+        adj[b].push_back(make_pair(c, a));
     }
     
-    for(int k = 1; k<=N; k++){
-        for(int i = 1; i<=N; i++){
-            for(int j = 1; j<=N; j++){
-                dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
-            }
-        }
+    for(int i = 1; i<= N; i++){
+        dis[i] = INF;
     }
     
-
-    for(int i = 1; i<=N; i++){
-        if(dis[1][i] <= K){
+    
+    shortestPath();
+    
+    for(int i = 1; i<= N; i++){
+        if(dis[i] <= K){
             cnt++;
         }
     }
     
     return cnt;
+    
+    
 }
