81dcdc04aff9181275ef4f0ce164fd1381c9899b
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-11-22 17:53:42 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,93 +1,97 @@
-// 14503 로봇청소기
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <cstdlib>
+#include <map>
+
 using namespace std;
 
-int n, m;
-int dir;
-int x, y, nx, ny;
-int cnt;
-int map[51][51];
-bool visited[51][51];
-bool Finish;
-int rotation;
 
-int Move[4][2] = {      // 현재 위치에   + : 전진 ,     - : 후진
-    {-1, 0},            // 북
-    {0, 1},             // 동
-    {1, 0},             // 남
-    {0, -1}             // 서
+int n, m;
+int node[51][51];
+bool isClean[51][51];
+
+int r, c, d;
+
+int cnt;
+
+int dir[4][2] = {
+    {-1, 0},
+    {0, 1},
+    {1, 0},
+    {0, -1},
 };
 
-
-void robot(){
-    visited[y][x] = 1;          // 시작 위치 방문 체크
-    cnt++;
-
-    while(y<n && y>=0 && x<m && x >= 0){
-        Finish = true;
-
-        for(int i = 0; i<4; i++){           // 청소 안 된 남은 칸 확인
-            ny = y + Move[i][0];
-            nx = x + Move[i][1];
-            if(map[ny][nx] == 0 && !visited[ny][nx]){     // 청소되지 않은 빈 칸 남은 경우
-                Finish = false;
-                break;
-            }
-        }
-
-        if(Finish){                         // 청소되지 않은 빈 칸 없는 경우 -> 후진
-            ny = y - Move[dir][0];
-            nx = x - Move[dir][1];
-
-            y = ny;
-            x = nx;
-
-            if(map[ny][nx] == 1){     // 벽 있어서 작동 멈춤
-                return;
-            }
-
-        }else{                                              // 청소되지 않은 빈 칸 남은 경우 -> 전진
-            while(rotation < 4){
-                dir = (dir + 3) % 4;                        // 반시계 회전
-                rotation++;
-
-                ny = y + Move[dir][0];                      // 전진
-                nx = x + Move[dir][1];
-
-                if(map[ny][nx] == 0 && !visited[ny][nx]){
-                    visited[ny][nx] = true;
-                    y = ny;
-                    x = nx;
-                    cnt++;
-                    rotation = 0;
-                    break;
-                }
-            
-            }                             
-
-        }
-    }
+bool canVisit(int nr, int nc){
+    return (nr >= 0 && nr < n && nc >= 0 && nc < m && node[nr][nc] == 0);
 }
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-
     cin >> n >> m;
-    cin >> y >> x >> dir;
+    cin >> r >> c >> d;
 
     for(int i = 0; i<n; i++){
         for(int j = 0; j<m; j++){
-            cin >> map[i][j];
+            cin >> node[i][j];
         }
     }
 
+    bool play = true;
 
-    robot();
-    cout<<cnt;
+    while(play){
+        if(!isClean[r][c]){
+            isClean[r][c] = true;   // 현재 칸 청소
+            cnt++;
+        }
 
+        int nr, nc;
+
+        bool flag = false;
+        for(int i = 0; i<4; i++){
+            nr = r + dir[i][0];
+            nc = c + dir[i][1];
+            if(canVisit(nr, nc) && !isClean[nr][nc]){
+                flag = true;        // 청소 안 된 빈칸 있음
+                break;
+            }
+        }
+
+        if(!flag){      // 후진
+            if(d == 0 && canVisit(r+1, c)){
+                r++;
+            }else if(d == 1 && canVisit(r, c-1)){
+                c--;
+            }else if(d == 2 && canVisit(r-1, c)){
+                r--;
+            }else if(d == 3 && canVisit(r, c+1)){
+                c++;
+            }else{
+                play = false;
+                // break;      // 작동 멈춰
+            }
+        }else{
+            // 회전
+            d = (d + 3) % 4;
+            if(d == 0 && !isClean[r-1][c] && canVisit(r-1, c)){
+                r--;
+            }else if(d == 1 && !isClean[r][c+1] && canVisit(r, c+1)){
+                c++;
+            }else if(d == 2 && !isClean[r+1][c] && canVisit(r+1, c)){
+                r++;
+            }else if(d == 3 && !isClean[r][c-1] && canVisit(r, c-1)){
+                c--;
+            }
+        }
+    }
+
+    cout << cnt;
 }
+
+
