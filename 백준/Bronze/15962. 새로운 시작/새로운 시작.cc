f02b5f73d81d8e0bb0d165da08727e6f014f6c6b
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-11-26 19:34:07 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,25 @@
+
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <cstdlib>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout<<"파이팅!!";
+}
+
+
+
