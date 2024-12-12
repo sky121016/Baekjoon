bc5fc1e320655ba86b5954e921cdb568d3f2428a
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-12-12 23:31:50 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,28 @@
+
+
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+   
+   cin>>a>>b;
+
+   cout<<b+b-a;
+}
