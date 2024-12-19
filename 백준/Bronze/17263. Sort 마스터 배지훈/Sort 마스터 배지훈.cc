0c99bd6f9289eb2adeff667f8a32b4becee81709
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-12-19 22:31:55 +0900

    [Bronze III] Title: Sort 마스터 배지훈, Time: 76 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Bronze/17263. Sort 마스터 배지훈/Sort 마스터 배지훈.cc
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+
+vector<int> a;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    int temp; 
+    for(int i = 0; i<n; i++){
+        cin >> temp;
+        a.push_back(temp);
+    }
+
+    sort(a.begin(), a.end());
+
+    cout << a[n - 1];
+}
