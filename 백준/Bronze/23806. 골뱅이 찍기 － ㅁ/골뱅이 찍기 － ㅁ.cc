68c50efa09cd6733bb5388bd488e1325404ba54e
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2024-12-24 21:23:50 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.cc
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<5*n; j++){
+            cout<<"@";
+        }
+        cout<<"\n";
+    }
+    
+    for(int i = 0; i<3*n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        for(int j = 0; j<3*n; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        cout<<"\n";
+    }
+
+   for(int i = 0; i<n; i++){
+        for(int j = 0; j<5*n; j++){
+            cout<<"@";
+        }
+        cout<<"\n";
+    }
+}
