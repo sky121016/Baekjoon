5f44f563d7826d24f51dac45cf325865535c8bc5
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-01-02 22:07:05 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.cc
@@ -0,0 +1,83 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        for(int j = 0; j<3*n; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        cout<<"\n";
+    }
+
+
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"@@@@@";
+        }
+        cout<<"\n";
+    }
+
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        for(int j = 0; j<3*n; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        cout<<"\n";
+    }
+
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"@@@@@";
+        }
+        cout<<"\n";
+    }
+
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        for(int j = 0; j<3*n; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j<n; j++){
+            cout<<"@";
+        }
+        cout<<"\n";
+    }
+
+}
