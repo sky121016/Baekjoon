e560ce10b3ddf672b72bc0ea384ead97444c4a62
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-02-26 22:17:48 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>n;
+
+    if(n == 0){
+        cout<<"YONSEI";
+    }else{
+        cout << "Leading the Way to the Future";
+    }
+    
+}
+
+
