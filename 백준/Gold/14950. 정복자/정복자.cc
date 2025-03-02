541716577f578390e410909b16177600b0719f7a
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-03-02 22:25:14 +0900

    [Gold IV] Title: 정복자, Time: 8 ms, Memory: 2728 KB -BaekjoonHub

---
+++ 백준/Gold/14950. 정복자/정복자.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+int m;
+int t;
+priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> cost;
+vector<int> parent;
+int line;
+
+int find(int v){
+    if(parent[v] == v){
+        return v;
+    }
+
+    return parent[v] = find(parent[v]);
+}
+
+int ans;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m >> t;
+
+    int a, b, c;
+    for(int i = 0; i<m; i++){
+        cin >> a >> b >> c;
+        cost.push(make_pair(c, make_pair(a, b)));
+    }
+
+    parent.push_back(0);
+    for(int i = 1; i<=n; i++){
+        parent.push_back(i);
+    }
+
+    while(true){
+        if(line == n-1){
+            break;
+        }
+
+
+        a = cost.top().second.first;
+        b = cost.top().second.second;
+        c = cost.top().first;
+
+        cost.pop();
+
+        a = find(a);
+        b = find(b);
+
+        if(a == b){
+            continue;
+        }else{
+            parent[a] = b;
+            ans += c;
+            ans += line*t;
+            line++;
+        }
+
+
+
+    }
+
+
+    cout << ans;
+}
