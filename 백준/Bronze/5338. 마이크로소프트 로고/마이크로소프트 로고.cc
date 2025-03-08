029044b2b37445b5ad0fffb0cc2cd60fb90f2d0d
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-03-08 23:08:57 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|\n";
+}
