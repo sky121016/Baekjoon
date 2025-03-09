6416e7b563a1e14232c730d5b9c79a6dcf2b2eb1
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-03-09 23:26:12 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+string a;
+string b;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b;
+
+    if(a.length() < b. length()){
+        cout<<"no";
+    }else{
+        cout<<"go";
+    }
+}
