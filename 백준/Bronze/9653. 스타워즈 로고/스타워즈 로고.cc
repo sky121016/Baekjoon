218a31eb846f16594fe10649949d64396a862201
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-03-16 23:40:51 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout<<"    8888888888  888    88888\n";
+    cout<<"   88     88   88 88   88  88\n";
+    cout<<"    8888  88  88   88  88888\n";
+    cout<<"       88 88 888888888 88   88\n";
+    cout<<"88888888  88 88     88 88    888888\n";
+    cout<<"\n";
+    cout<<"88  88  88   888    88888    888888\n";
+    cout<<"88  88  88  88 88   88  88  88\n";
+    cout<<"88 8888 88 88   88  88888    8888\n";
+    cout<<" 888  888 888888888 88  88      88\n";
+    cout<<"  88  88  88     88 88   88888888\n";
+
+}
+
