96f7b2a89cf0007c85a51188ad1bcd6adee0b356
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-03-21 23:00:16 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout<<"  ___  ___  ___\n";
+    cout<<"  | |__| |__| |\n";
+    cout<<"  |           |\n";
+    cout<<"   \\_________/\n";
+    cout<<"    \\_______/\n";
+    cout<<"     |     |\n";
+    cout<<"     |     |\n";
+    cout<<"     |     |\n";
+    cout<<"     |     |\n";
+    cout<<"     |_____|\n";
+    cout<<"  __/       \\__\n";
+    cout<<" /             \\\n";
+    cout<<"/_______________\\\n";
+}
+
+
