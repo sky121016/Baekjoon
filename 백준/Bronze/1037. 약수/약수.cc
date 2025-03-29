b2c84a27ce1272a1a1a7012ed4f2d21052549aba
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-03-29 23:22:04 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+vector<int> a;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    int temp;
+    for(int i = 0; i<n; i++){
+        cin >> temp;
+        a.push_back(temp);
+    }
+
+    sort(a.begin(), a.end());
+
+    cout << a[0] * a[a.size() - 1];
+}
+
