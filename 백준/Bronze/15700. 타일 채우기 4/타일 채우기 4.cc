67613c53091bfe3c2be382d8b546fd09916da15e
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-04-07 23:06:03 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+long long n, m;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>n>>m;
+
+    cout << (n * m) / 2;
+}
