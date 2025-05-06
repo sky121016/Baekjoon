e8b0af70ce4b2e10154640c9eae87df7a94892cb
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-05-06 23:56:54 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    int c, d;
+
+    cin >> a >> c >> b >> d;
+
+    cout << a * c + b * d;
+}
