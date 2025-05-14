0c4b21293bedb15b8d544f71e54cb0ee301af399
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-05-14 23:42:35 +0900

    [Bronze IV] Title: Kuber, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20833. Kuber/Kuber.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+
+    int t = (n * (n + 1)) / 2;
+    cout << t*t << "\n";
+
+
+}
