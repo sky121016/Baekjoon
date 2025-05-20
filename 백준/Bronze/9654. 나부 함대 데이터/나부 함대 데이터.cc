d639aff162e022f6dfc90b74141e9f38a3bf39ff
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-05-20 23:50:52 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int k, a, d;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout<<"SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4   ";
+    
+}
