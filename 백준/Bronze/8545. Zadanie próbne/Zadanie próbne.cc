3b6b5f2e28ea9aa9dfab0d568745705a3d102dc8
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-05-26 23:44:53 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+string s;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> s;
+
+    cout << s[2] << s[1] << s[0];
+}
+
+
