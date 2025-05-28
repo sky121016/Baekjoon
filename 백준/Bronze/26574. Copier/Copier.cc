c9536646af55923481515adce0adfa0edb10043f
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-05-28 23:19:07 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int n;
+int a;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for(int i = 0; i<n; i++){
+        cin >> a;
+        cout << a << " " << a << "\n";
+    }
+}
+
