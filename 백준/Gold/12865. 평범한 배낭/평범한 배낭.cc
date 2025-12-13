156329b91c465c6108ec5195c084cb8bcc27aeb8
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2025-12-13 17:19:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 36 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,45 +1,45 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <cstdlib>
+#include <map>
 
 using namespace std;
 
-int N, K;
-int dp[101][101010];
-vector<int> W;
-vector<int> V;
+int dp[101][100001];
+vector<int> w;
+vector<int> v;
+int n, k;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> N >> K;
+    cin >> n >> k;
 
-    W.push_back(0);
-    V.push_back(0);
+    int a, b;
+    w.push_back(0);
+    v.push_back(0);
 
-    int w, v;
-    for(int i = 0; i < N; i++){
-        cin >> w >> v;
-        W.push_back(w);
-        V.push_back(v);
+    for(int i = 0; i<n; i++){
+        cin >> a >> b;
+        w.push_back(a);
+        v.push_back(b);
     }
 
-    for(int i = 1; i<=N; i++){
-        for(int j = 0; j<=K; j++){
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=k; j++){
             dp[i][j] = dp[i-1][j];
-
-            if(j-W[i] >= 0){
-                dp[i][j] = max(dp[i - 1][j - W[i]] + V[i], dp[i][j]);
+            if(j-w[i] >= 0){
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
             }
         }
     }
 
-
-
-    cout << dp[N][K];
+    cout << dp[n][k];
 }
-
 
