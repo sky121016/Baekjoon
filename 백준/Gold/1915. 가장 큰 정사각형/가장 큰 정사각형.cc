0e75f318a57383f0a3caf4ccae04eec27203e2bb
Sumin Hong <102947171+sky121016@users.noreply.github.com>
2026-01-10 17:18:13 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 16 ms, Memory: 6912 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,15 +1,16 @@
-// 1915
+// 11053
 #include<iostream>
+#include<string>
 #include<vector>
-#include<stack>
 #include<algorithm>
+#include<queue>
 
 using namespace std;
 
 int n, m;
-char map[1001][1001];
 int dp[1001][1001];
-int maxSize;
+char map[1001][1001];
+int Max = 0;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -28,27 +29,23 @@ int main(){
     for(int i = 1; i<=n; i++){
         for(int j = 1; j<=m; j++){
             if(map[i][j] == '1'){
-                if(map[i-1][j-1] == '1' && map[i-1][j] == '1' && map[i][j-1] == '1'){
-                    dp[i][j] = min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
-                    if(maxSize<dp[i][j]){
-                        maxSize = dp[i][j];
+                if(map[i-1][j] == '1' && map[i][j-1] == '1' && map[i-1][j-1] == '1'){
+                    dp[i][j] = min(dp[i-1][j-1],min(dp[i - 1][j], dp[i][j - 1])) + 1;
+    
+                    if(dp[i][j] > Max){
+                        Max = dp[i][j];
                     }
                 }else{
                     dp[i][j] = 1;
-                    if(maxSize<dp[i][j]){
-                        maxSize = dp[i][j];
+                    if(dp[i][j] > Max){
+                        Max = dp[i][j];
                     }
+            
                 }
-            }else{
-                dp[i][j] = 0;
             }
         }
     }
 
-    cout<<maxSize * maxSize;
 
-
-
-
+    cout << Max * Max;
 }
-
